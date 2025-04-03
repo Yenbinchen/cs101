11e39ced6bb12641c7ed9fed7980413bd5c232b5
Yenbinchen <yen.bin.chen@gmail.com>
2025-04-03 23:36:32 +0800

    Update lotto.c

--- HW3/lotto.c
+++ HW3/lotto.c
@@ -5,7 +5,7 @@
 int is_winning_line(const char *line, const char *winning[], int win_count) {
     for (int i = 0; i < win_count; i++) {
         if (strstr(line, winning[i]) != NULL) {
-            return 1; 
+            return 1;
         }
     }
     return 0; 
@@ -20,13 +20,13 @@ int main() {
         return 1;
     }
 
-    
+   
     const char *winning_numbers[] = { " 02", " 04", " 06" };
     int win_count = sizeof(winning_numbers) / sizeof(winning_numbers[0]);
 
     char line[256];
     while (fgets(line, sizeof(line), infile)) {
-        
+       
         if (line[0] == '[') {
             if (is_winning_line(line, winning_numbers, win_count)) {
                 fputs(line, outfile); 
