b4ee6b85b0aef6cff3929dd423b45e79fe97cbc1
Yenbinchen <yen.bin.chen@gmail.com>
2025-04-03 23:09:28 +0800

    Create Aa.c

---
+++ HW3/Aa.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void replace_all(char* str, char oldChar, char newChar){
+    for(int i=0;str[i]!='\0';i++){
+        if(str[i]==oldChar){
+            str[i]=newChar;
+        }
+    }
+    
+}
+
+int main(){
+    char str[100], oldChar = 'a', newChar = 'A';
+    scanf("%s", str);
+
+    replace_all(str, oldChar, newChar);
+    printf("%s", str);
+
+    return 0;
+}
